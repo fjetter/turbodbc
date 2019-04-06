cf3d18360c3fa0b6c22dc09fdc6d6106ee51e562
fjetter <fjetter@users.noreply.github.com>
2019-04-06 14:46:12 +0200

    pyenv stuff

--- travis/scripts/install.sh
+++ travis/scripts/install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -exo pipefail
-if [ "${TURBODBC_USE_CONDA}" != "yes" ]; then
+if [ "$TRAVIS_OS_NAME" == "osx"]; then
     eval "$(pyenv init -)"
     eval "$(pyenv virtualenv-init -)"
     pyenv activate turbodbc
+fi
+
+if [ "${TURBODBC_USE_CONDA}" != "yes" ]; then
     pip install numpy==1.14.5 pyarrow==$TURBODBC_ARROW_VERSION six twine pytest-cov coveralls pandas
 fi

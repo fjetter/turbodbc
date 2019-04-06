050e7f74cc5e434ee09364d9695c04cd87dc6583
fjetter <fjetter@users.noreply.github.com>
2019-04-06 14:16:27 +0200

    pyenv all over the place

--- travis/scripts/install.sh
+++ travis/scripts/install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -euxo pipefail
 if [ "${TURBODBC_USE_CONDA}" != "yes" ]; then
+    eval "$(pyenv init -)"
+    eval "$(pyenv virtualenv-init -)"
+    pyenv activate turbodbc
     pip install numpy==1.14.5 pyarrow==$TURBODBC_ARROW_VERSION six twine pytest-cov coveralls pandas
 fi
